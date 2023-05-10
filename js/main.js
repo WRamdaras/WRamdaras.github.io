0898949719116d6b4d27757e4514db1fcfefb0d4
WRamdaras <112637524+WRamdaras@users.noreply.github.com>
2023-05-10 18:30:38 +0200

    about me changed
    links changed

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const ghLink = document.querySelector('.bi-github');
+const sourceCode = document.querySelector('.source-btn');
+const sourceCodeOne = document.querySelector('.source-btn-one');
+const sourceCodeTwo = document.querySelector('.source-btn-two');
+const linkedinBtn = document.querySelector('.bi-linkedin');
+
+ghLink.addEventListener('click', function(link){
+    link.preventDefault();
+  window.open("https://github.com/WRamdaras");
+
+});
+
+sourceCode.addEventListener('click', function(link){
+    link.preventDefault();
+  window.open("https://github.com/WRamdaras/Shoe-Site");
+
+});
+
+sourceCodeOne.addEventListener('click', function(link){
+    link.preventDefault();
+  window.open("https://github.com/ROCMondriaanTIN/sd22-themaweek-2-t1k-WRamdaras");
+
+});
+
+sourceCodeTwo.addEventListener('click', function(link){
+    link.preventDefault();
+  window.open("https://github.com/ROCMondriaanTIN/sd22-design-p2-portfolio-WRamdaras");
+
+});
+
+linkedinBtn.addEventListener('click', function(link){
+    link.preventDefault();
+  window.open("https://nl.linkedin.com/");
+
+});
+
