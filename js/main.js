1b092564ea70364fe40d43628cd7554278138795
WRamdaras <112637524+WRamdaras@users.noreply.github.com>
2023-06-11 16:57:13 +0200

    added preview button link for javascript

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
-let ghLink = document.querySelector('.bi-github');
-let sourceCodeNetflix = document.querySelector('.source-btn');
-let sourceCodeOne = document.querySelector('.source-btn-one');
-let sourceCodeTwo = document.querySelector('.source-btn-two');
-let sourceCodeThree = document.querySelector('.source-btn-three');
-let sourceCodeFour = document.querySelector('.source-btn-four');
-let sourceCodeFive = document.querySelector('.source-btn-five');
-let linkedinBtn = document.querySelector('.bi-linkedin');
-let = previewNetflix = document.querySelector(".preview-btn")
+// let sourceCodeNetflix = document.querySelector(".source-btn");
+let sourceCodeOne = document.querySelector(".source-btn-one");
+let sourceCodeTwo = document.querySelector(".source-btn-two");
+let sourceCodeThree = document.querySelector(".source-btn-three");
+let sourceCodeFour = document.querySelector(".source-btn-four");
+let sourceCodeFive = document.querySelector(".source-btn-five");
+// let = previewNetflix = document.querySelector(".preview-btn")
 let = previewBtnOne = document.querySelector(".preview-btn-one")
 let = previewBtnTwo = document.querySelector(".preview-btn-two")
 let = previewBtnThree = document.querySelector(".preview-btn-three")
 let = previewBtnFour = document.querySelector(".preview-btn-four")
 let = previewBtnFive = document.querySelector(".preview-btn-five")
+// let ghLink = document.querySelector(".bi-github");
+// let linkedinBtn = document.querySelector(".bi-linkedin");
 
-ghLink.addEventListener('click', function(link){
-    link.preventDefault();
-  window.open("https://github.com/WRamdaras");
 
-});
-
-sourceCodeNetflix.addEventListener('click', function(link){
+let sourceCodeNetflix = document.querySelector(".source-btn");
+sourceCodeNetflix.addEventListener("click", function(link){
     link.preventDefault();
   window.open("https://github.com/WRamdaras/netflix");
-
 });
-
-sourceCodeOne.addEventListener('click', function(link){
-    link.preventDefault();
-  window.open("https://github.com/ROCMondriaanTIN/sd22-themaweek-2-t1k-WRamdaras");
-
-});
-
-sourceCodeTwo.addEventListener('click', function(link){
-    link.preventDefault();
-  window.open("https://github.com/ROCMondriaanTIN/sd22-design-p2-portfolio-WRamdaras");
-
-});
-
-linkedinBtn.addEventListener('click', function(link){
-    link.preventDefault();
-  window.open("https://nl.linkedin.com/in/wiresh-ramdaras-954a09276");
-
-});
-
+let = previewNetflix = document.querySelector(".preview-btn")
 previewNetflix.addEventListener("click", function(link){
   link.preventDefault();
   window.open("https://wramdaras.github.io/netflix/");
 })
+
+sourceCodeOne.addEventListener("click", function(link){
+    link.preventDefault();
+  window.open("https://github.com/ROCMondriaanTIN/sd22-themaweek-2-t1k-WRamdaras");
+});
+
+sourceCodeTwo.addEventListener("click", function(link){
+    link.preventDefault();
+  window.open("https://github.com/ROCMondriaanTIN/sd22-design-p2-portfolio-WRamdaras");
+});
+
+let ghLink = document.querySelector(".bi-github");
+ghLink.addEventListener("click", function(link){
+    link.preventDefault();
+  window.open("https://github.com/WRamdaras");
+});
+
+let linkedinBtn = document.querySelector(".bi-linkedin");
+linkedinBtn.addEventListener("click", function(link){
+    link.preventDefault();
+  window.open("https://nl.linkedin.com/in/wiresh-ramdaras-954a09276");
+});
+
