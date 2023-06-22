3ea31d95cacb3e679eb8c152a93c47b10767e93a
WRamdaras <112637524+WRamdaras@users.noreply.github.com>
2023-06-22 14:40:55 +0200

    contact reset added

--- js/main.js
+++ js/main.js
@@ -99,3 +99,9 @@ window.onscroll = function () {
   const progressHeight = (window.pageYOffset / totalHeight) * 100;
   progressBar.style.width = progressHeight + "%";
 };
+
+// reset contact
+let contactReset = document.querySelector(".contact-form")
+contactReset.addEventListener("submit",   function(){
+  this.reset();
+})
