77a16119c3ac560f17a83ce2961858a08d635375
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2024-04-04 17:25:32 +0200

    updated links

--- js/main.js
+++ js/main.js
@@ -1,51 +1,3 @@
-// Dashboard
-let sourceCodeDashboard = document.querySelector(".source-btn");
-sourceCodeDashboard.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://github.com/WRamdaras/dashboard");
-});
-let = previewBtnDashboard = document.querySelector(".preview-btn");
-previewBtnDashboard.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://wramdaras.github.io/dashboard/index.html");
-});
-
-// Webcatalogus
-let sourceCodeWebcatalogus = document.querySelector(".source-btn-one");
-sourceCodeWebcatalogus.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://github.com/WRamdaras/Webcatalogus");
-});
-let = previewBtnWebcatalogus = document.querySelector(".preview-btn-one");
-previewBtnWebcatalogus.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://wramdaras.github.io/Webcatalogus/");
-});
-
-// Tic Tac Toe
-let sourceCodeBKE = document.querySelector(".source-btn-four");
-sourceCodeBKE.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://github.com/WRamdaras/TicTacToe");
-});
-let = previewBtnBKE = document.querySelector(".preview-btn-four");
-previewBtnBKE.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://wramdaras.github.io/TicTacToe/");
-});
-
-// Higher Lower
-let sourceCodeHogerLager = document.querySelector(".source-btn-five");
-sourceCodeHogerLager.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://github.com/WRamdaras/HigherLower");
-});
-let = previewBtnHogerLager = document.querySelector(".preview-btn-five");
-previewBtnHogerLager.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://wramdaras.github.io/HigherLower/");
-});
-
 // AnimeHub
 let sourceCodeAnimeHub = document.querySelector(".source-btn-six");
 sourceCodeAnimeHub.addEventListener("click", function (link) {
@@ -70,26 +22,6 @@ previewBtnFBshirts.addEventListener("click", function (link) {
   // window.open("https://wramdaras.github.io/HigherLower/");
 });
 
-// Github link
-let ghLink = document.querySelector(".bi-github");
-ghLink.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://github.com/WRamdaras");
-});
-
-// Linkedin link
-let linkedinBtn = document.querySelector(".bi-linkedin");
-linkedinBtn.addEventListener("click", function (link) {
-  link.preventDefault();
-  window.open("https://nl.linkedin.com/in/wiresh-ramdaras-954a09276");
-});
-
-// Inspiration link
-let inspirationLink = document.querySelector(".inspiration-link");
-inspirationLink.addEventListener("click", function(link){
-  link.preventDefault();
-  window.open("https://sbechoe.github.io/");
-})
 
 // Progress bar
 const progressBar = document.querySelector(".progress-bar");
